f53e1bf2e42e5bd92ba5d263e9c0674c43141c5f
Mazepin <IgorMazepin@users.noreply.github.com>
2020-12-03 21:57:02 +0300

    feat: add project

---
+++ src/Homework1.1/Homework1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TMS_03-12");
+        }
+    }
+}
