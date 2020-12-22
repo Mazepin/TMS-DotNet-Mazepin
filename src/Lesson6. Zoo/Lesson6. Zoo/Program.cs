e50873112fdb90c1586b3fd0fd1bf27879e2eaf2
Igor <75262127+Mazepin@users.noreply.github.com>
2020-12-22 10:13:41 +0300

    feat: add Console.Clear

---
+++ src/Lesson6. Zoo/Lesson6. Zoo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson6._Zoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
