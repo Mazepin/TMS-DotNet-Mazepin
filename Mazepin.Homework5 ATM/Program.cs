a9ccf0d65c4bd04428d3a3cdf4be25fcde0eed20
Igor <75262127+Mazepin@users.noreply.github.com>
2021-01-23 22:31:41 +0300

    init: add ATM get, put money. Show balance

---
+++ Mazepin.Homework5 ATM/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Mazepin.Homework5_ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ATM put, get money
+
+            Atm atm = new Atm();
+            atm.BalanceCard += InfoBalance;
+
+            Console.WriteLine($"Hello! Your curently balance is: 0 BYN");
+
+            Console.Write("\nEnter the amount of money to put:");
+            decimal amountMoneyPut = decimal.Parse(Console.ReadLine());
+            atm.PutMoney(amountMoneyPut);
+            Console.WriteLine("===============");
+
+            Console.Write("\nEnter the amount of money to get:");
+            decimal amountMoneyGet = decimal.Parse(Console.ReadLine());
+            atm.GetMoney(amountMoneyGet);
+            Console.WriteLine("===============");
+
+            Console.Write("\nСlick B to view balance "); // условно, можно нажать любую клавишу
+            string balanceInfo = Console.ReadLine();
+            atm.GetInfo();
+
+            Console.ReadKey();
+        }
+        public static void InfoBalance(decimal amount, string operation)
+        {
+            Console.WriteLine($"Operation {operation}. Balance: {amount} BYN");
+        }
+    }
+}
+
+
+
